b08e7b7e7bd2009894611aa1e0d5f9bc0fac587f
Savin97 <47434031+Savin97@users.noreply.github.com>
2026-01-01 10:43:19 +0200

    Project Initialization

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+from pipeline.pipeline import run_pipeline
+
+def main():
+    print("Application started")
+    run_pipeline()
+    print("Pipeline execution completed")
+
+if __name__ == "__main__":
+    main()
---
+++ pipeline/pipeline.py
@@ -0,0 +1,5 @@
+# import pipeline.pipeline_
+
+def run_pipeline():
+    print("Running the pipeline...")
+    return 
