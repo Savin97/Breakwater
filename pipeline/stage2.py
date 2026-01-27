0aaf50c67014067992c770235ad2e0bba75194da
Savin97 <47434031+Savin97@users.noreply.github.com>
2026-01-27 12:23:55 +0200

    27/1/26 sector features

--- pipeline/stage2.py
+++ pipeline/stage2.py
@@ -37,19 +37,5 @@ def stage2(stage1_df):
     for feature in feature_steps:
         df = feature(df)
     
-
-    # Sector Features
-    # sector_drift_and_vol = (
-    #     df.groupby(['sector', 'date'])
-    #         .agg(
-    #             sector_drift_60d=('drift_60d', 'mean'), # Average of past 60 days across every stock in the same sector on that day
-    #             sector_vol_10d=('vol_10d', 'mean'), # Typical daily volatility in the sector in the last 10 days, percentages
-    #             sector_vol_30d=('vol_30d', 'mean')
-    #         )
-    #         .groupby('sector')
-    #         .shift(1)
-    #         .reset_index()
-    # )
-    # df = df.merge( sector_drift_and_vol, on=['sector', 'date'], how='left')
-
+    
     return df
