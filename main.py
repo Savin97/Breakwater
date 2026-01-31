8e9a224788b4bc2b4c3c5b5a17e9d96b81e5056f
Savin97 <47434031+Savin97@users.noreply.github.com>
2026-01-31 18:27:12 +0200

    31/1/26 sync check

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pipeline.pipeline import run_pipeline
 def main():
     print("--------------------\nRunning pipeline...\n--------------------\n")
     run_pipeline()    
+    #tweak
     print("Pipeline execution completed.\n--------------------")
     
 if __name__ == "__main__":
