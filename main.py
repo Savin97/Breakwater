2f0dfd58364f2369443e4691fd75a9ec6d772105
Savin97 <47434031+Savin97@users.noreply.github.com>
2026-02-01 20:09:57 +0200

    minor thing

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from pipeline.pipeline import run_pipeline
 def main():
     print("--------------------\nRunning pipeline...\n--------------------\n")
     run_pipeline()    
-    #tweak
     print("Pipeline execution completed.\n--------------------")
     
 if __name__ == "__main__":
