06a5b13cad55f6a37ce83eb17566afd0783c817b
Michael <savin992@gmail.com>
2026-02-09 16:21:30 +0200

    9/2/26 Tested explosiveness feature, looks good. Continuing back testing. Added comments in stage1 for improved fetching

--- pipeline/stage1.py
+++ pipeline/stage1.py
@@ -29,8 +29,62 @@ def stage1():
     """
     directory_checks()   
     warnings.filterwarnings('ignore')
+    def check_stock_prices():
+        from pathlib import Path
+        pass
+        # if Path("data/stock_prices.csv").exists():
+        """
+            check_prices_file()
+            if the file already exists, check if it has
+                1. the correct columns: stock, date, price
+                2. data from STOCKS_START_DATE to STOCKS_END_DATE
+                if it has full data - pd.read_csv and return 
+                if it has partial data:
+                    - if it has the correct columns but only partial date range, fetch the rest and append
+                    - if it doesn't -  delete and re-fetch
+            
 
-    #janky_solution()
+        """
+        # else:
+            # if the file doesn't exist - fetch new and save 
+            # stock_prices = fetch_stock_prices(provider=PRICES_PROVIDER)
+
+    def check_earnings_dates():
+        from pathlib import Path
+        pass
+        # if Path("data/earnings_dates.csv").exists():
+        """
+            check_earnings_file()
+            if the file already exists, check if it has
+                1. the correct columns: stock, date, earnings_date
+                2. data from STOCKS_START_DATE to STOCKS_END_DATE
+                if it has full data - pd.read_csv and return 
+                if it has partial data:
+                    - if it has the correct columns but only partial date range, fetch the rest and append
+                    - if it doesn't -  delete and re-fetch
+        """
+        # else:
+            # if the file doesn't exist - fetch new and save 
+            # earnings_dates = fetch_earnings_dates()
+
+    def check_sector_data():
+        from pathlib import Path
+        pass
+        # if Path("data/sector_data.csv").exists():
+        """
+            check_sector_data_file()
+            if the file already exists, check if it has
+                1. the correct columns: stock, date, sector, sub_sector, market_cap, beta
+                2. data from STOCKS_START_DATE to STOCKS_END_DATE
+                if it has full data - pd.read_csv and return 
+                if it has partial data:
+                    - if it has the correct columns but only partial date range, fetch the rest and append
+                    - if it doesn't -  delete and re-fetch
+        """
+        # else:
+            # if the file doesn't exist - fetch new and save 
+            # sector_market_cap_beta_df = fetch_sector_data()
+
 
     stock_prices = fetch_stock_prices(provider=PRICES_PROVIDER)
     earnings_dates = fetch_earnings_dates()
@@ -45,10 +99,8 @@ def stage1():
     stock_prices = stock_prices.sort_values("date")
     earnings_dates = earnings_dates.sort_values("earnings_date")
     
-
     df = merge_prices_earnings_dates(stock_prices, earnings_dates) # df that holds stock prices, earnings dates, EPS data merged
 
-    
     eps_data = fetch_eps()
     # TODO: EPS is fetched but not merged now. ignored for as Im focusing on pre-earnings features
     #df = merge_main_df_with_eps_df(df, eps_data)
