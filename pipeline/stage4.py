cbf63f5b277113db1060be44a798b500093e5e2a
Savin97 <47434031+Savin97@users.noreply.github.com>
2026-04-13 23:42:25 +0300

    13/4/26 still testing scoring

--- pipeline/stage4.py
+++ pipeline/stage4.py
@@ -39,7 +39,10 @@ def stage4(stage3_df):
         stage4_df = f(stage4_df)
     if stage4_df is None:
         raise ValueError("\n---ERROR! Stage 4 Returned None.---\n")
+    
+    print(stage4_df["momentum_pressure_regime"].value_counts(dropna=False).head(20))
     print("Building parquet...")
     stage4_df.to_parquet("output/full_df.parquet")
+
     print("Stage 4 DONE")
     return stage4_df
